65ed0c90ee22719064f72a56b06a0414f3c8ec5d
strikertushar19 <tushardogra19@gmail.com>
2024-08-13 21:19:45 +0530

    added mcqs unit2 physical chemistry 10

--- app/jee/maths/page.js
+++ app/jee/maths/page.js
@@ -1,122 +1,57 @@
 import React from "react";
-import { mathsques } from "../../question/question";
-
+const mathsques = [
+  {
+    title: "Binomial Theorem And Its Simple Applications",
+    href: "binomialTheoremAndItsSimpleApplications",
+  },
+  {
+    title: "Complex Numbers And Quadratic Equations",
+    href: "complexNumbersAndQuadraticEquations",
+  },
+  { title: "Coordinate Geometry", href: "coordinateGeometry" },
+  { title: "Differential Equations", href: "differentialEquations" },
+  { title: "Integral Calculus", href: "integralCalculus" },
+  { title: "Matrices And Determinants", href: "matricesAndDeterminants" },
+  {
+    title: "Permutations And Combinations",
+    href: "permutationsAndCombinations",
+  },
+  { title: "Probability", href: "probability" },
+  { title: "Sets Relations And Functions", href: "setsRelationsAndFunctions" },
+  { title: "Statistics And Probability", href: "statisticsAndProbability" },
+  { title: "Trigonometry", href: "trigonometry" },
+  {
+    title: "Limits, Continuity And Differentiability",
+    href: "limitContinuityAndDifferentiability",
+  },
+  { title: "3D Geometry", href: "threeDimensionalGeometry" },
+  { title: "Vector Algebra", href: "vectorAlgebra" },
+];
 const MathsPage = () => {
   return (
-    <div className="pt-24">
-      <div className="container mx-auto">
+    <div className="pt-24 h-screen flex flex-row">
+      <div className="h-full w-1/2">
+        <img
+          src="/maths.jpg"
+          alt="Maths"
+          className="h-full w-full object-cover"
+        />
+      </div>
+      <div className="h-full w-1/2 flex flex-col justify-center p-20">
         <h1 className="text-3xl font-bold mb-4">Maths Chapters</h1>
-        {/* <ul>
-          {mathsques.map((item, index) => (
+        <ul>
+          {mathsques.map((ques, index) => (
             <li key={index}>
-              <h3 className="text-red-500">{item.question}</h3>
-              <p className="text-black">Answer: {item.answer}</p>
+              <h3 className="text-red-500">
+                <a
+                  className="text-blue-700 underline"
+                  href={`/jee/maths/${ques.href}`}
+                >
+                  {ques.title}
+                </a>
+              </h3>
             </li>
           ))}
-        </ul> */}
-        <ul>
-          <li>
-            <h3 className="text-red-500">
-              <a className="my-2" href="/jee/maths/binomialTheoremAndItsSimpleApplications">
-                Binomial Theorem And Its Simple Applications
-              </a>
-            </h3>
-          </li>
-          <li>
-            <h3 className="text-red-500">
-              <a className="my-2" href="/jee/maths/complexNumbersAndQuadraticEquations">
-              Complex Numbers And Quadratic Equations
-              </a>
-            </h3>
-          </li>
-          <li>
-            <h3 className="text-red-500">
-              <a className="my-2" href="/jee/maths/coordinateGeometry">
-              Coordinate Geometry
-              </a>
-            </h3>
-          </li>
-          <li>
-            <h3 className="text-red-500">
-              <a className="my-2" href="/jee/maths/differentialEquations">
-              Differential Equations
-              </a>
-            </h3>
-          </li>
-          <li>
-            <h3 className="text-red-500">
-              <a className="my-2" href="/jee/maths/integralCalculus">
-              Integral Calculus
-              </a>
-            </h3>
-          </li>
-          <li>
-            <h3 className="text-red-500">
-              <a className="my-2" href="/jee/maths/matricesAndDeterminants">
-              Matrices And Determinants
-              </a>
-            </h3>
-          </li>
-          <li>
-            <h3 className="text-red-500">
-              <a className="my-2" href="/jee/maths/permutationsAndCombinations">
-              Permutations And Combinations
-              </a>
-            </h3>
-          </li>
-          <li>
-            <h3 className="text-red-500">
-              <a className="my-2" href="/jee/maths/probability">
-              Probability
-              </a>
-            </h3>
-          </li>
-          <li>
-            <h3 className="text-red-500">
-              <a className="my-2" href="/jee/maths/setsRelationsAndFunctions">
-              Sets Relations And Functions
-              </a>
-            </h3>
-          </li>
-          <li>
-            <h3 className="text-red-500">
-              <a className="my-2" href="/jee/maths/StatisticsAndProbability">
-              Statistics And Probability
-              </a>
-            </h3>
-          </li>
-          <li>
-            <h3 className="text-red-500">
-              <a className="my-2" href="/jee/maths/trigonometry">
-              Trigonometry
-              </a>
-            </h3>
-          </li>
-          <li>
-            <h3 className="text-red-500">
-              <a className="my-2" href="/jee/maths/limitContinuityAndDifferentiability">
-              Limits, Continuity And Differentiability
-              </a>
-            </h3>
-          </li>
-          <li>
-            <h3 className="text-red-500">
-              <a className="my-2" href="/jee/maths/ThreeDimensionalGeometry">
-              3D Geometry
-              </a>
-            </h3>
-            
-          </li>
-          <li>
-            <h3 className="text-red-500">
-              <a className="my-2" href="/jee/maths/VectorAlgebra">
-              Vector Algebra
-              </a>
-            </h3>
-          </li>
-          
-
-          
         </ul>
       </div>
     </div>
