4142a61dc51debc499105685f5b346c8b61c474d
Arun Bhardwaj <iamarun5603@gmail.com>
2024-08-25 18:21:55 +0530

    done the same configuration for physics and maths like chemistry

---
+++ app/jee/physics/Unit14/mcqData.js
@@ -0,0 +1,90 @@
+const mcqs = [
+    {
+      section: "A",
+      question: "The element having no neutron in the nucleus of its atom is",
+      options: ["Hydrogen", "Nitrogen", "Helium", "Boron"],
+      answer: "Hydrogen",
+    },
+    {
+      section: "A",
+      question: "Example of super octet molecule is :",
+      options: ["SF6", "PCl5", "IF7", "All of these"],
+      answer: "All of these",
+    },
+    {
+      section: "A",
+      question: "Correct order of bond energy of C–O bond is :",
+      options: [
+        "CO3^2- > CO2 > CO",
+        "CO2 > CO > CO3^2-",
+        "CO > CO2 > CO3^2-",
+        "None of these.",
+      ],
+      answer: "CO > CO2 > CO3^2-",
+    },
+    {
+      section: "A",
+      question:
+        "Arrange the following ions in the order of decreasing X–O bond length where X is the central atom",
+      options: [
+        "CIO4^- , SO4^2- , PO4^3- , SiO4^4-",
+        "SiO4^4- , PO4^3- , SO4^2- , CIO4^-",
+        "SiO4^4- , PO4^3- , CIO4^- , CIO4^2-",
+        "SiO4^2- , SO4^2- , PO4^3- , CIO4^-",
+      ],
+      answer: "SiO4^4- , PO4^3- , SO4^2- , CIO4^-",
+    },
+  
+    {
+      section: "A",
+      question:
+        "Which of the following overlaps gives a sigma bond with x as internuclear axis?",
+      options: ["pz and pz", "s and pz", "s and px", "dxy^2 and dxy^2"],
+      answer: "s and px",
+    },
+    {
+      section: "A",
+      question: "The structure of F2SeO is analogous to :",
+      options: ["SO3", "CIO3^-", "XeO3", "(B) and (C) both"],
+      answer: "(B) and (C) both",
+    },
+    {
+      section: "A",
+      question: "In which of the following N is in the sp2 hybridisation state",
+      options: ["(CH3)3N", "CH2 = NH", "CH3CN", "NO2+"],
+      answer: "CH2 = NH",
+    },
+    {
+      section: "A",
+      question: "In H2SO4 molecule",
+      options: [
+        "S-atom is sp3d2 hybridised and there are 4 lone pair electrons on ‘O’ atoms.",
+        "S-atom is sp3 hybridised and there are no lone pair of electrons in the molecule.",
+        "S-atom is sp3 hybridised and there are 8 lone pair electrons on the ‘O’ atoms.",
+        "S-atom is sp2 hybridised and these are 8 lone pair electrons ‘O’ atoms",
+      ],
+      answer:
+        "S-atom is sp3 hybridised and there are 4 lone pair electrons on ‘O’ atoms.",
+    },
+    {
+      section: "A",
+      question:
+        "In which of the following pairs hybridisation of the central atoms are different ?",
+      options: [
+        "ClF3 , ClF3O",
+        "ClF3O, ClF3O2",
+        "[ClF2O]+ , [ClF4O]-",
+        "[ClF4O]- , [XeOF4]",
+      ],
+      answer: "[ClF4O]- , [XeOF4]",
+    },
+    {
+      section: "A",
+      question:
+        "All the following species have all their bond lengths identical except :",
+      options: ["AsF3", "AsF4^-", "AsF4+", "AsF6^-"],
+      answer: "AsF3",
+    },
+  ];
+  export default mcqs;
+  
--- app/jee/physics/page.js
+++ app/jee/physics/page.js
@@ -1,23 +1,97 @@
 import React from "react";
-import {physicsques} from "../../question/question"; 
-
+const physicsques = [
+  {
+    title: "Physics and Measurement",
+    href: "Unit1",
+  },
+  {
+    title: "Kinematics",
+    href: "Unit2",
+  },{
+    title: "Laws of Motion",
+    href: "Unit3",
+  },{
+    title: "Work, Energy, and Power",
+    href: "Unit4",
+  },{
+    title: "Rotational Motion",
+    href: "Unit5",
+  },{
+    title: "Gravitation",
+    href: "Unit6",
+  },{
+    title: "Properties of Solids and Liquids",
+    href: "Unit7",
+  },{
+    title: "Thermodynamics",
+    href: "Unit8",
+  },{
+    title: "Kinetic Theory of Gases",
+    href: "Unit9",
+  },{
+    title: "Oscillations and Waves",
+    href: "Unit10",
+  },{
+    title: "Electrostatics",
+    href: "Unit11",
+  },{
+    title: "Current Electricity",
+    href: "Unit12",
+  },{
+    title: "Magnetic Effects of Current and Magnetism",
+    href: "Unit13",
+  },{
+    title: "Electromagnetic Induction and Alternating Currents",
+    href: "Unit14",
+  },{
+    title: "Electromagnetic Waves",
+    href: "Unit15",
+  },{
+    title: "Optics",
+    href: "Unit16",
+  },{
+    title: "Dual Nature of Matter and Radiation",
+    href: "Unit17",
+  },{
+    title: "Atoms and Nuclei",
+    href: "Unit18",
+  },{
+    title: "Electronic Devices",
+    href: "Unit19",
+  },{
+    title: "Communication System",
+    href: "Unit20",
+  },
+];
 const PhysicsPage = () => {
   return (
-    <div className="pt-24">
-      <div className="container mx-auto">
-        <h1 className="text-3xl font-bold mb-4">Chemistry Questions</h1>
+    <div className="pt-24 h-screen flex flex-row">
+      <div className="h-full w-1/2">
+        <img
+          src="/Physics.jpg"
+          alt="Physics"
+          className="h-full w-full object-cover"
+        />
+      </div>
+      <div className="h-full w-1/2 flex flex-col justify-center p-20">
+        <h1 className="text-3xl font-bold mb-4">Physics Chapters</h1>
         <ul>
-          {physicsques.map((item, index) => (
+          {physicsques.map((ques, index) => (
             <li key={index}>
-              {/* <p className="font-bold">Question {index + 1}:</p> */}
-              <h3 className="text-red-500">{item.question}</h3>
-              <p className="text-black">Answer: {item.answer}</p>
+              <h3 className="text-red-500">
+                <a
+                  className="text-blue-700 underline"
+                  href={`/jee/physics/${ques.href}`}
+                >
+                  {ques.title}
+                </a>
+              </h3>
             </li>
           ))}
         </ul>
       </div>
     </div>
   );
-}
+};
 
 export default PhysicsPage;
